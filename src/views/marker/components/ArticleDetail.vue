cf50e2a1b7ded11f83ab694048ca462f91b22985
webhaohao <734714852@qq.com>
2021-03-02 01:09:04 +0800

    fix marker

--- src/views/marker/components/ArticleDetail.vue
+++ src/views/marker/components/ArticleDetail.vue
@@ -149,9 +149,11 @@ export default {
       this.postForm = Object.assign({}, defaultForm)
     }
     this.getCatories()
+
     // Why need to make a copy of this.$route here?
     // Because if you enter this page and quickly switch tag, may be in the execution of the setTagsViewTitle function, this.$route is no longer pointing to the current page
     // https://github.com/PanJiaChen/vue-element-admin/issues/1221
+
     this.tempRoute = Object.assign({}, this.$route)
   },
   methods: {
